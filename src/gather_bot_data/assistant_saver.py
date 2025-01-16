de80412d56ca627ee646b65409f201eda870d7e1
Vicente Pareja <vicente.pareja@uc.cl>
2025-01-15 21:17:45 -0300

    fix(assistant_saver) little correction on the path in with the assistant are being saved.

--- src/gather_bot_data/assistant_saver.py
+++ src/gather_bot_data/assistant_saver.py
@@ -5,5 +5,5 @@ class AssistantSaver:
         self.assistants_directory = PATH_ASSISTANTS_DIRECTORY
 
     def save(self, assistant_name: str, assistant_id: str):
-        with open(self.assistants_directory + f"_{assistant_name}.txt", "w", encoding="utf-8") as f:
+        with open(self.assistants_directory + f"/{assistant_name}.txt", "w", encoding="utf-8") as f:
             f.write(f"{assistant_name, assistant_id}\n")
